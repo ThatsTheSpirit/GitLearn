c55857b3fa8559ec5e5cc996458b8c2b27c06bfa
Atmos <1999bag@gmail.com>
2024-07-04 18:57:45 +0300

    feat(Flat.cs): added constructor

--- Git/Flat.cs
+++ Git/Flat.cs
@@ -5,5 +5,12 @@
         private uint number;
         private byte citizens;
         private byte floor;
+
+        public Flat(uint number, byte citizens = 1, byte floor = 1)
+        {
+            this.number = number;
+            this.citizens = citizens;
+            this.floor = floor;
+        }
     }
 }
